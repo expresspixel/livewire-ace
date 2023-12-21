e372629ca3c8af58442337470cf8c1c85847cab8
artisanbr <github@artisan.dev.br>
2023-12-20 21:41:12 -0300

    first commit

---
+++ src/AceEditorBase.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace ArtisanLabs\LivewireAce;
+
+use Livewire\Component;
+
+class AceEditorBase extends Component
+{
+    public string $editorView;
+    public ?string $editorLayout = '';
+
+    public string $value = '';
+    public string $language = '';
+
+    protected $listeners = ['languageChanged'];
+
+    public function mount()
+    {
+        $this->editorView = $this->editorView
+            ?? "livewire-ace::editor";
+    }
+
+    public function render()
+    {
+        if (empty($this->editorLayout)) {
+            return view($this->editorView);
+        }
+
+        return view($this->editorView)
+            ->layout($this->editorLayout);
+    }
+
+    /**
+     * Event listener for programmatically changing the
+     * session language mode.
+     *
+     * @param string $lang
+     */
+    public function languageChanged(string $lang)
+    {
+        $this->language = $lang;
+        $this->emit('changeLanguage');
+    }
+
+    /**
+     * Callback function to listen for value updates.
+     */
+    public function updatedValue(string $value)
+    {
+    }
+
+    /**
+     * Callback function to listen for language updates.
+     */
+    public function updatedLanguage(string $lang)
+    {
+        $this->emitSelf('changeLanguage', $lang);
+    }
+}
---
+++ src/LivewireAceServiceProvider.php
@@ -0,0 +1,75 @@
+<?php
+
+
+namespace ArtisanLabs\LivewireAce;
+
+use Illuminate\Support\ServiceProvider;
+
+class LivewireAceServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . "/../resources/views", "livewire-ace");
+        $this->mergeConfigFrom(__DIR__ . "/../config/config.php", 'ace');
+
+        \Blade::directive('livewireAceScripts', function () {
+
+            $defaultTheme = config('ace.defaults.theme');
+            $defaultLanguage = config('ace.defaults.language');
+
+            $scripts = [
+                '<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js" crossorigin="anonymous"></script>',
+                <<<HTML
+<script>
+    function EditorData() {
+        return {
+            data: {},
+            init(element, wire, lw) {
+                if (!ace) {
+                    console.error('Ace editor is not available in window.');
+                    return;
+                }
+
+                const editor = ace.edit(element);
+                editor.session.setValue(lw.value);
+
+                if ("$defaultTheme") {
+                    editor.setTheme('ace/theme/' + "$defaultTheme");
+                }
+
+                if ("$defaultLanguage") {
+                    editor.session.setMode('ace/mode/' + "$defaultLanguage");
+                }
+
+                editor.session.on('change', () => {
+                    lw.value = editor.getValue();
+                });
+
+                lw.on('changeLanguage', () => {
+                   console.log(lw.language);
+                });
+
+                lw.on('changeTheme', () => {
+                   console.log(lw.theme);
+                });
+            }
+        }
+    }
+</script>
+HTML
+            ];
+
+            $toAsset = fn($name) => '<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/' . $name . '.min.js" crossorigin="anonymous"></script>';
+
+            $assets = array_merge(
+                $scripts,
+                array_map(fn($mode) => $toAsset('mode-' . $mode), config('ace.assets.modes')),
+                array_map(fn($mode) => $toAsset('theme-' . $mode), config('ace.assets.themes')),
+            );
+
+            return implode(PHP_EOL, $assets);
+        });
+    }
+
+}
